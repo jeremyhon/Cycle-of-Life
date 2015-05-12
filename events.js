6a3ac1112b09d7c9f160337bc4e55561318f3b71
Jeremy Hon <jeremy.hon.gy@gmail.com>
2015-05-12 17:41:18 +0800

    added debug meter, events js, partial implementation of events. No implementation of choices

---
+++ events.js
@@ -0,0 +1,14 @@
+//the events database. store all events here.
+var EventDB = [
+    {
+        index: 0, 
+        notif: "This is a sample event.", 
+        choices: ["Default choice here"],
+        reqs: []
+    },{
+        index: 1,
+        notif: "This is another event",
+        choices: ["Choice 1", "Choice 2"],
+        reqs: []
+    }
+];
--- game.js
+++ game.js
@@ -1,6 +1,9 @@
 var adultExists = false;
 var onEvent = false;
-var child = new Person("defaultChild");
+var eventTimer = 100;
+var eventMax = 10;
+var debugText = "";
+var child = new Person("defaultChild", 0, 0.1,[]);
 
 // Run UI update code every 10ms
 window.setInterval(function () {
@@ -8,64 +11,88 @@ window.setInterval(function () {
     if(!onEvent){
         update(child);
     }
-    
+    eventTimer--;
     //update everything
     $('#child-age-display').text(Math.floor(child.currentAge));
+    $('#debug').text(debugText);
+    jQuery('#child-notifications').html(embedNL(child.notifsHistory));
 }, 10);
 
 function update(person){
     //increment age
     person.currentAge += 0.01;
     //roll for events
-    rollEvent(person);
+    if(eventTimer < 0){
+        eventTimer = eventMax;
+        rollEvent(person);
+    }
 }
 
 function rollEvent(person) {
     //generate a random number
     var newRandom = Math.random();
+    debugText = newRandom;
     //check against the event multiplier
-    if(newRandom < eventMultiplier){
+    if(newRandom < person.eventMultiplier){
         //jackpot!
         triggerEvent(person);
     }
 }
 
 function triggerEvent(person){
+    person.onEvent = true;
     //pick a random event from the list of allowable events.
     //TODO: trim the list of allowable events! Right now it allows everything lol
-    var randEventIndex = allowableEvents[Math.random() * person.allowableEvents.length];
+    var randEventIndex = person.allowableEvents[Math.floor(Math.random() * person.allowableEvents.length)];
     var newEvent = EventDB[randEventIndex];
 
-    //pull the previous text
-    var prevNotifs = $('#notifications').text();
-    var newNotifs = prevNotifs + "\n\n" + newEvent.notif;
+    //push the new notification into the queue
+    person.notifsHistory.push(newEvent.notif);
+    //trim the notifications
+    if(person.notifsHistory.length > 5){
+        person.notifsHistory.shift();
+    }
+    //create the button
+    createButtons(newEvent, person);
 }
 
-function Event(index, notif, choices, reqs){
-    this.index = index;
-    this.notif = notif;
-    // choices and reqs should be arrays. Please pass arrays into these parameters.
-    this.choices = choices;
-    this.reqs = reqs;
+function createButtons(event, person){
+    //the button to add
+    var test = $('<button/>',
+    {
+        text: 'Test',
+        click: function () { alert('hi'); }
+    });
+
+    //create a div and insert the button inside
+    var parent = $('<div></div>').children().append(test).end();
+    //add the div to the event holder.
+    if(person.currentAge<25){
+        $('#child-event-holder').append(parent);
+    } else {
+        //$('#adult-event-holder').append(parent);
+    }
 }
 
-function Person(name, currentAge = 0, eventMultiplier = 0.1, allowableEvents = []){
+function Person(name, currentAge, eventMultiplier, allowableEvents){
     this.name = name;
     this.currentAge = currentAge;
     this.eventMultiplier = 0.1;
-    //if allowableEvents is default, insert everything
+    //if allowableEvents is empty, insert everything
     if(allowableEvents.length == 0){
-        for (var i = 0; i <EventDB.length - 1; i++) {
+        for (var i = 0; i < EventDB.length; i++) {
             allowableEvents += [i];
         };
     }
-    this.notifsHistory = "";
+    this.allowableEvents = allowableEvents;
+    this.notifsHistory = [];
 }
 
-//the events database. store all events here.
-var EventDB = [
-    new Event(0, 
-        "This is a sample event.", 
-        ["Default choice here"])
-]
-
+function embedNL(lines) {
+    var htmls = [];
+    var tmpDiv = jQuery(document.createElement('div'));
+    for (var i = 0 ; i < lines.length ; i++) {
+        htmls.push(tmpDiv.text(lines[i]).html());
+    }
+    return htmls.join("<br>");
+}
